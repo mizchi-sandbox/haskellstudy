3c95a2bdb3467848e60b8fc609e7406baabfbed4
Koutarou Chikuba <miz404@gmail.com>
2012-04-24 18:06:38 +0900

    modulize

--- QuickSort.hs
+++ QuickSort.hs
@@ -1,6 +1,7 @@
-qsort :: [Int] -> [Int]
-qsort [] = []
-qsort (x:xs) =
-  (qsort (filter (<x) xs)) ++ [x] ++ (qsort (filter (>=x) xs))
+module Main where
+  qsort :: [Int] -> [Int]
+  qsort [] = []
+  qsort (x:xs) =
+    (qsort (filter (<x) xs)) ++ [x] ++ (qsort (filter (>=x) xs))
 
-main = print (qsort [5,6,1,35,324,1342,234,2,432])
+  main = print (qsort [5,6,1,35,324,1342,234,2,432])
